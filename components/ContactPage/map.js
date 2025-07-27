4f911fd6983a58a9dabc486c29bf22cbdaf66b4a
Dawood Ahmed <76210505+Dawod-Ahmed@users.noreply.github.com>
2025-07-27 13:42:55 +0400

    Update map.js

--- components/ContactPage/map.js
+++ components/ContactPage/map.js
@@ -7,7 +7,7 @@ function Maps() {
   return (
     <>
       <div className="map_parent ">
-        <iframe className="map" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d904.108403691303!2d55.1748916!3d24.9853778!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f728ca5ad27d1%3A0x83f8b86e6a48c938!2sSchon%20Business%20Park!5e0!3m2!1sen!2sae!4v1692432794049!5m2!1sen!2sae" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe className="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3611.7787258278954!2d55.2210124!3d25.1431707!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f6a2b7a4959c5%3A0x1e9ad1c89e20a6c7!2sAl%20Masraf%20-%20Sheikh%20Zayed%20Branch!5e0!3m2!1sar!2sae!4v1749722163601!5m2!1sar!2sae" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
       
       
       </div>
